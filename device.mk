55261688d44961d037fac1a324738c2c323db10e
Vladimir <acbkaa@gmail.com>
2016-03-17 22:15:20 +0300

    update twrp 3.0

--- device.mk
+++ device.mk
@@ -1,5 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product-if-exists, vendor/asus/anthias/anthias-vendor.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/asus/anthias/tetra-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/anthias/overlay
 
@@ -12,10 +16,6 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.anthias:root/fstab.anthias \
-    $(LOCAL_PATH)/init.recovery.anthias.rc:root/init.recovery.anthias.rc
 
 $(call inherit-product, build/target/product/full.mk)
 
