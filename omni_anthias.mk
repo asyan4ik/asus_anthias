0146c68b1fdabc46297f721a94e2e12d311adfa2
Vladimir <acbkaa@gmail.com>
2016-03-17 22:17:40 +0300

    update twrp 3.0

--- omni_anthias.mk
+++ omni_anthias.mk
@@ -1,20 +1,36 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Release name
-PRODUCT_RELEASE_NAME := anthias
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+#
+# This file is the build configuration for a full Android
+# build for grouper hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit device configuration
+# Inherit from those products. Most specific first.
 $(call inherit-product, device/asus/anthias/device.mk)
+# This is where we'd set a backup provider if we had one
+#$(call inherit-product, device/sample/products/backup_overlay.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := anthias
+
+# Discard inherited values and use our own instead.
 PRODUCT_NAME := omni_anthias
+PRODUCT_DEVICE := anthias
 PRODUCT_BRAND := Asus
 PRODUCT_MODEL := anthias
-PRODUCT_MANUFACTURER := ZenWatch
